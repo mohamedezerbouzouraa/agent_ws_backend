ffeb03637bca8e5c8760ccb566609de210e2237b
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-06-30 16:25:25 +0100

    Add AgentState class with message annotations

--- agents/state.py
+++ agents/state.py
@@ -2,5 +2,6 @@ from typing import Annotated, Sequence
 from langchain_core.messages import BaseMessage
 from langgraph.graph.message import add_messages
 from typing_extensions import TypedDict
+
 class AgentState(TypedDict):
     messages: Annotated[Sequence[BaseMessage], add_messages]
