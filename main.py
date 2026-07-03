5a5cfe08a741bcc5e826ebd10516000af969d26d
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-07-03 23:19:56 +0100

    execution + user interaction

--- main.py
+++ main.py
@@ -11,8 +11,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_methods=["*"],
-    allow_headers=["*"],
-)
+    allow_headers=["*"],)
 
 @app.get("/health")
 async def health():
